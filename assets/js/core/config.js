b5cdd75ad7e5209a8de3fea4ab92f9787fc68561
wzmsdev-ops <wzms.dev@gmail.com>
2026-06-26 20:45:12 +0900

    Update config.js

--- assets/js/core/config.js
+++ assets/js/core/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  APP_NAME: '의료장비 · 정도관리 시스템 (데모)',
+  APP_NAME: '관리이지',
 
   SUPABASE_URL: 'https://iaxeizhoelhwitsllrmw.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlheGVpemhvZWxod2l0c2xscm13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0MTU1MDMsImV4cCI6MjA5Nzk5MTUwM30.PO-cclqvkO1meBmd3oTVBe6m2v7SZS2KctIP__xbfOc',
